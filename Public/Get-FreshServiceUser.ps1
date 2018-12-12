5719cd1a2e9806fc78092751c431cd9c94fe240c
PrzemyslawKlys <pklys@evotec.pl>
2018-12-12 12:30:23 +0100

    Updates to Get-FreshServiceUser

--- Public/Get-FreshServiceUser.ps1
+++ Public/Get-FreshServiceUser.ps1
@@ -1,10 +1,53 @@
 function Get-FreshServiceUser {
+    [CmdletBinding(DefaultParameterSetName = 'Count')]
     param(
-        $Connection
+        $Connection,
+        [Parameter(ParameterSetName = 'Count')][nullable[int]] $Count = 50,
+        [Parameter(ParameterSetName = 'All')] [switch] $All
     )
-    $Question = "itil/requesters.json" # ?state=all" # GET example: get all users (limited to first 50)"
-    $URL = "$($Connection.FullDomain)/$Question"
+    Begin {
+        switch ($PsCmdlet.ParameterSetName) {
+            'All' { $Count = $null; break}
+            'Count' { break }
+        }
+    }
+    Process {
+        [int] $PageCount = 1
+        [Array] $Feed = $null
+        while ($True) {
+            $Question = "itil/requesters.json/?page=$PageCount" ##?state=all" # GET example: get all users (limited to first 50)"
 
-    $Data = Invoke-RestMethod -Method Get -Uri $URL -Headers $Connection.HTTPHeaders
-    return $Data
+            [System.Uri] $URL = "$($Connection.FullDomain)/$Question"
+            #Write-Verbose "Get-FreshServiceUser - Url: $URL"
+
+            $Output = Invoke-RestMethod -Method Get -Uri $URL -Headers $Connection.HTTPHeaders
+            Write-Verbose "Get-FreshServiceUser - Output Count: $($Output.Count)"
+            if ($Output.Count -eq 0) {
+                break;
+            } else {
+                $Feed += $Output.User
+            }
+            Write-Verbose "Get-FreshServiceUser - Total Count: $($Feed.Count), Expected Count: $Count"
+
+            if ($All) {
+
+            } elseif ($Count) {
+                if ($Feed.Count -ge $Count) {
+                    break
+                }
+            } else {
+                # Shouldn't happen really
+                break
+            }
+
+            $PageCount++
+        }
+    }
+    End {
+        if ($All) {
+            return $Feed
+        } else {
+            return $Feed | Select-Object -First $Count
+        }
+    }
 }
